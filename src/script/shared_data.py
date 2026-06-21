c485dbbba3c90f91c4a0f2bc9120c71397972d8b
samdeupta <gupta.samprit2006@gmail.com>
2026-06-21 02:12:45 -0700

    chore: Add symbol to atomic num map in shared data file and implement its data loader

--- src/script/shared_data.py
+++ src/script/shared_data.py
@@ -1,5 +1,28 @@
+import csv
+from pathlib import Path
+
 from decay_chain_dag import DecayChainDAG
 
 
-batemanState_cache  = {}
-decay_chain_graph   = DecayChainDAG()
+# ----- Shared Data Structures/Caches --------------------
+decay_chain_graph                           = DecayChainDAG()
+symbol_to_atomic_num_map : dict[str, int]   = {}
+
+
+# ----- Data Loaders & Data Structure Builders --------------------
+def load_symbol_to_atomic_num_map():
+    """
+    """
+
+    path = Path(__file__).parent.parent / "data" / "elements.csv"
+
+    with open(path, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+
+        for row in reader:
+            symbol_to_atomic_num_map[row["symbol"]] = int(row["atomic_num"])
+
+
+def build_decay_chain_graph():
+    """
+    """
